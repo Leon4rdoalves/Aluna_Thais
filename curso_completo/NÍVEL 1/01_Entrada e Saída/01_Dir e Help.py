03db325c91c9c3b5a72e15163d597401c53d7e86
Leoanardo Alves <leon4rdoalvess@gmail.com>
2020-07-21 20:09:54 -0300

    Subindo conteúdo de aula

---
+++ curso_completo/NÍVEL 1/01_Entrada e Saída/01_Dir e Help.py
@@ -0,0 +1,9 @@
+'''
+Dir >>> Retorna atributos, funções e metodos
+
+Help >>> Retorna a documentação
+'''
+
+
+#help(print)
+
---
+++ curso_completo/NÍVEL 1/01_Entrada e Saída/02_Print_Input.py
@@ -0,0 +1,16 @@
+'''
+Print >>> Saída, mostra os dados na tela do usuário.
+Input >>> Receber dados do usuário do sistema.
+'''
+
+
+
+nome = input('Qual seu nome: ')
+
+# Sintaxe antig.
+print('Muito prazer, {}'.format(nome))
+
+# Sintaxe atual
+print(f'Muito prazer, {nome}')
+
+
