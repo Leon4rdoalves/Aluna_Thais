68c75a36d7ccbc233d323eb3c7ef9a9c373ec4c6
Leoanardo Alves <leon4rdoalvess@gmail.com>
2020-07-21 19:52:12 -0300

    Concluído

---
+++ curso_completo/nivel1/02_Variáveis e Tipos de Dados/02_tipo_float.py
@@ -0,0 +1,18 @@
+"""
+Float >>> Números reais ou decimais. Importante ressaltar que sempre
+deve ser separados por ponto e não vírgula.
+"""
+
+# correto
+num = 155.2564
+
+# errado
+num1 = 155,2564
+
+
+print(num)
+print(type(num))
+
+# Note que o tipo do dado na variável num1, é uma tupla e não um boolean
+
+
---
+++ curso_completo/nivel1/02_Variáveis e Tipos de Dados/03_tipo_booleano.py
@@ -0,0 +1,30 @@
+"""
+Booleano >>> 2 constantes - Verdadeiro e falso
+"""
+
+''' 
+Estudo de caso: Suponha que um app permite teste de 7 dias e pergunta ao usuário
+se o pagamento para uso completo foi realizado, caso sim, libera em confiança a assinatura.
+'''
+
+
+usuario = False    # Ele ainda não pagou pela versão completa, portanto seu status é false
+
+status = usuario   # Variável responsavel por guardar este e outros usuários.
+
+# Solicitando resposta do usuário e alterando em letra maiúscula.
+pg = input('O pagamento foi feito? [S para sim | N para não]: ').upper()
+
+
+# testando se o pagamento foi feito, note que é necessário que a resposta esteja em letras maiúsculas.
+if pg == 'S':
+    status = (not usuario)
+
+
+# Saída personalizada com o status do usuário.
+print(f'Usuário com versão completa: {status}')
+
+
+
+# Maneira direta de testar booleano >>>  print('a' in 'maria')
+
