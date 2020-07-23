25cbb86115029e428c081ed4ba454838bf4fde88
Leoanardo Alves <leon4rdoalvess@gmail.com>
2020-07-22 23:48:33 -0300

    Update

--- curso_completo/nivel1/01_Entrada e Saída/02_Print_Input.py
+++ curso_completo/nivel1/01_Entrada e Saída/02_Print_Input.py
@@ -1,16 +1,33 @@
-'''
-Print >>> Saída, mostra os dados na tela do usuário.
-Input >>> Receber dados do usuário do sistema.
-'''
+"""
+Print >>> Mostrar dado
+Input >>> Obter um dado do usário
+
+nome = 'leonardo alvES'.title().strip().split()
+
+#print('Muito prazer, {}'.format(nome))
+
+print(f'Muito prazer, {nome[1]}')
+
+
+
+Com split: Gerando índice
+      0               1
+L e o n a r d o   a l v e s
+
+
+0 1 2 3 4 5 6 7 8 9 10 11 12 13     Python sempre cria um índice interno quando inserimos um dado.
+L e o n a r d o   a l v e s
 
 
 
 nome = input('Qual seu nome: ')
 
-# Sintaxe antig.
+
+Sintaxe antig.
 print('Muito prazer, {}'.format(nome))
 
-# Sintaxe atual
-print(f'Muito prazer, {nome}')
+
+Sintaxe atual
+print(f'Muito prazer, {nome}')"""
 
 
